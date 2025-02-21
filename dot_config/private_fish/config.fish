e78477e06d137ef68c64d1f640d1016d24fa7a19
Jack Wenyoung <dlwxxxdlw@gmail.com>
2025-02-21 21:46:01 +0800

    feat(yay): add packages install declaratively stuff
    This commit contains the following changes:
        A .chezmoidata/packages.yaml
        A .chezmoiignore
        M dot_config/private_fish/config.fish
        A run_onchange-install-packages.sh.tmpl

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,29 +2,30 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-abbr lg 'lazygit'
-abbr lj 'lazyjj'
-abbr czi 'chezmoi'
-abbr md 'mkdir'
-abbr ls 'eza'
+abbr lg lazygit
+abbr lj lazyjj
+abbr czi chezmoi
+abbr md mkdir
+abbr ls eza
 abbr ll 'eza -l'
 abbr la 'eza -a'
 abbr lt 'eza -T'
 abbr jjbm 'jj bookmark s -r @- main'
+abbr gpom 'git push -u origin main'
 abbr efish 'nvim ~/.config/fish/config.fish'
 abbr refish 'source ~/.config/fish/config.fish'
 
 function enproxy
-  set -xg ALL_PROXY http://localhost:7897
-  set -xg HTTP_PROXY http://localhost:7897
-  set -xg HTTPS_PROXY http://localhost:7897
-  echo "Enable proxies"
+    set -xg ALL_PROXY http://localhost:7897
+    set -xg HTTP_PROXY http://localhost:7897
+    set -xg HTTPS_PROXY http://localhost:7897
+    echo "Enable proxies"
 end
 function deproxy
-  set -e ALL_PROXY
-  set -e HTTP_PROXY
-  set -e HTTPS_PROXY
-  echo "Disable proxies"
+    set -e ALL_PROXY
+    set -e HTTP_PROXY
+    set -e HTTPS_PROXY
+    echo "Disable proxies"
 end
 starship init fish | source
 zoxide init fish | source
